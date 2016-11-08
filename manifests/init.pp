1f8470d25398cc97351bc566316df2231aeea455
Konsta <konsta.olli@ymail.com>
2016-11-08 15:16:03 +0200

    Laitoin testi manifestin puppettia varten

---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+class testi{
+	Package { ensure => "installed", allowcdrom => "true",}
+	
+	package { "gedit": }
+	package { "apache2": }
+	package { "ssh": }
+	package { "libapache2-mod-php": }
+
+	file {"/var/www/html/index.html":
+	content => "toimiihan se?",
+}
+
+	file { '/etc/apache2/mods-enabled/userdir.conf':
+	ensure => 'link',
+	target => '../mods-available/userdir.conf',
+	notify => Service["apache2"],
+}
+
+	file { '/etc/apache2/mods-enabled/userdir.load':
+	ensure => 'link',
+	target => '../mods-available/userdir.load',
+	notify => Service["apache2"],
+}
+
+	service {"apache2":
+	ensure => "true",
+	enable => "true",
+
+}
+	file { "/etc/skel/public_html":
+	ensure => 'directory',
+}
+}
